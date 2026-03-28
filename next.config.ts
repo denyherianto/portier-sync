d09b4d45b682653e393fe03cb9c101da776025a3
Deny Herianto <deny.hrnt@gmail.com>
2026-03-28 19:46:02 +0700

    chore: add dockerfile and compose

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   serverExternalPackages: ['better-sqlite3'],
+  output: 'standalone',
 };
 
 export default nextConfig;
